42ca139aeb3a1a98539196257bcc05d61a13735d
Jakub 'beamerkun' Sokół <beamer.kun@gmail.com>
2013-02-24 16:45:19 +0100

    WIP - 2 char history now works, but somewhat disappointing results

--- main.cpp
+++ main.cpp
@@ -7,56 +7,62 @@
 #include <iostream>
 #include <time.h>
 #include <ctype.h>
-#define MIN 1
+#define MIN 3
 #define MAX 15
 
 
 class NameGenerator{
 	std::map< char, std::map< char, std::map< char, int > > > mat;
-	
+
 	public:
-	
+
 	void update( std::string str ){
 		if( str.length() <= 0 )
 			return;
-		for( int i=0; i <= str.length() - 2; ++i ){
+		int* n = &mat[255][str[0]][str[1]];
+		*n += 1;
+		for( int i = 0; i <= str.length() - 2; ++i ){
 			int* num = &mat[tolower(str[i])][str[i+1]][str[i+2]];
 			*num += i;
 		}
 	}
 
 	std::string generate(){
-		char temp;
+		char last, curr, next;
 		std::string result;
 
-	
-
-		if( mat.empty() )
-			return "";
-
 		do {
-			temp = rand()%26 + 97;
-			std::cout << "reroll...\n";
-		} while ( mat.find(temp) == mat.end() );
+			last = 255;
 
-		do {
 			std::vector<char> list;
-			result.push_back( temp );
-			for( auto i = mat[temp].begin(); i != mat[temp].end(); ++i ){
-				auto matrix = i->second;
-				for( auto j = matrix.begin(); j != matrix.end(); ++j)
-					for( int k = j->second; k > 0; --k ){
-						list.push_back( j->first );
-					}
+			for( auto i = mat[last].begin(); i != mat[last].end(); ++i ){
+				list.push_back(i->first);
 			}
-			if( list.size() != 0 )
-				temp = list.at( rand()%list.size() );
-			else
-				temp = '\0';
-		} while ( temp != '\0' );
-		if( result.size() < MIN || result.size() > MAX )
-			result = generate();
-		result.at(0) = toupper( result.at(0) );
+			curr = list.at( rand()%list.size() );
+
+			do {
+				result.push_back( last );
+
+				for( auto i = mat[last].begin(); i != mat[last].end(); ++i ){
+					auto matrix = i->second;
+					for( auto j = matrix.begin(); j != matrix.end(); ++j){
+						for( int k = j->second; k > 0; --k )
+							list.push_back(j->first);
+					}
+				}
+
+				if( list.empty() )
+					next = '\0';
+				else
+					next = list.at( rand()%list.size() );
+
+				last = curr;
+				curr = next;
+				list.clear();
+			} while (last != '\0' || curr != '\0');
+		} while ( result.size() < MIN || result.size() > MAX );
+
+		result = result.substr( 1, result.size() -1 );
 		return result;
 	}
 
